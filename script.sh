ac880c5988177f666a8bb6c295c3837d6b499464
Devloper <devloper01@gmail.com>
2024-10-24 17:48:19 +0530

    script

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Download and install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
+
+# Install Ansible
+sudo yum install ansible* -y
+ansible --version
+
+# Install the community.aws collection
+ansible-galaxy collection install community.aws
+ansible-galaxy collection list | grep community.aws
+
+# Install necessary dependencies (boto3 and botocore)
+pip3 install boto3 botocore
+pip3 list | grep -E 'boto3|botocore'
